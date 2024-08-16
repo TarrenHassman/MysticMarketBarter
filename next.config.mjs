8e2cde05ef9b851f659840b3bbe408fd7e3e0368
TarrenHassman <tarren.m.hassman@gmail.com>
2024-08-16 10:52:56 -0700

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["euc.li"]
-    }
+  images: {
+    domains: ["euc.li"],
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
